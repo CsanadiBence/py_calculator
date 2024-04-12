51bca12f7bb9639a3b1ccd53ab8971dd6622630b
CsanadiBence <127303953+CsanadiBence@users.noreply.github.com>
2024-04-12 08:53:42 +0200

    kk

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from pyscript import document
     
+def torles(event):
+    document.querySelector("#szam1").value = ""
+    document.querySelector("#szam2").value = ""
+    document.querySelector("#eredmeny").textContent = ""
+    document.querySelector("#muvelet").selectedIndex = ""
