25b2133ea78ea0114adc708c6f36f92ffa274341
CsanadiBence <127303953+CsanadiBence@users.noreply.github.com>
2024-04-12 08:57:09 +0200

    bruuh

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from pyscript import document
-    
+def szamit(event):
+    szam1 = float(document.querySelector("#szam1").value)
+    szam2 = float(document.querySelector("#szam2").value)
+    muvelet = document.querySelector("#muvelet").value
+    document.querySelector("#eredmeny").textContent = ""
+
+  
 def torles(event):
     document.querySelector("#szam1").value = ""
     document.querySelector("#szam2").value = ""
