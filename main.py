a920bf4f5e99ce889381ea6b8c62a37394643d00
fodorgabor <127303784+fodorgabor@users.noreply.github.com>
2024-04-12 09:28:32 +0200

    kk

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ def szamit(event):
     szam1 = float(document.querySelector("#szam1").value)
     szam2 = float(document.querySelector("#szam2").value)
     muvelet = document.querySelector("#muvelet").value
-    document.querySelector("#eredmeny").textContent = ""
+    
+    res=0
+    
+    if muvelet=="osszeadas":
+        res=szam1+szam2
+    elif muvelet=="kivonás":
+        res=szam1-szam2
+    elif muvelet=="szorzás":
+        res=szam1*szam2
+    elif muvelet=="osztás":
+        res=szam1/szam2
+    
+    document.querySelector("#result-number").textContent = res
 
   
-def torles(event):
+def torol(event):
     document.querySelector("#szam1").value = ""
     document.querySelector("#szam2").value = ""
-    document.querySelector("#eredmeny").textContent = ""
-    document.querySelector("#muvelet").selectedIndex = ""
+    document.querySelector("#result-number").textContent = "0"
+    document.querySelector("#muvelet").selectedIndex = 0
